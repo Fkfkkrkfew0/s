d544ceecc658150a7fc22bf7c6da197108412b80
Fkfkkrkfew0 <44093001+Fkfkkrkfew0@users.noreply.github.com>
2018-11-11 22:07:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,55 +1,17 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-console.log("Scrpit By ♛ 『DE』 NovritschPro#6808");
-
-
-client.on("ready", () => {
-let channel =     client.channels.get("506353680509960193")
-setInterval(function() {
-channel.send(`فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي فدرالي`);
-}, 50)
-})
-
-
-
-
-
-
-
-
-
-client.user.acceptInvite(invite)
-
-
-
-
-
-
-
-
-
-
-
-
-        var prefix = "$";
-    client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-  if (command == "spam") {
-if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR**');
-
-
-   message.channel.sendMessage(args.join("  "))
-   message.delete()
-  }
- });
-
- 
- 
-client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
+client.on("ready", async  => {
+setInterval(function(){
+client.channels.find('id', ' 511254120485879848 ').setName("「");
+client.channels.find('id', ' 511254120485879848 ').setName("「 Wel");
+client.channels.find('id', ' 511254120485879848 ').setName("「 Welco");
+client.channels.find('id', ' 511254120485879848 ').setName("「 Welcome");
+client.channels.find('id', ' 511254120485879848 ').setName("「 Welcome To");
+client.channels.find('id', ' 511254120485879848 ').setName("「 Welcome To D");
+client.channels.find('id', ' 511254120485879848 ').setName("「 Welcome To DJ");
+client.channels.find('id', ' 511254120485879848 ').setName("「 Welcome To DJ D");
+client.channels.find('id', ' 511254120485879848 ').setName("「 Welcome To DJ Da");
+client.channels.find('id', ' 511254120485879848 ').setName("「 Welcome To DJ DaM");
+client.channels.find('id', ' 511254120485879848 ').setName("「 Welcome To DJ DaM Se");
+client.channels.find('id', ' 511254120485879848 ').setName("「 Welcome To DJ DaM Serv ");
+client.channels.find('id', ' 511254120485879848 ').setName("「 Welcome To DJ DaM Server 」");
+  }, 3000);
+});

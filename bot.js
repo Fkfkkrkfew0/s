f3ca5892cc165264c2c2f3592c56f755842d32a5
Fkfkkrkfew0 <44093001+Fkfkkrkfew0@users.noreply.github.com>
2018-11-15 14:01:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,29 @@
-client.on("ready", async  => {
-setInterval(function(){
-client.channels.find('id', ' 511254120485879848 ').setName("「");
-client.channels.find('id', ' 511254120485879848 ').setName("「 Wel");
-client.channels.find('id', ' 511254120485879848 ').setName("「 Welco");
-client.channels.find('id', ' 511254120485879848 ').setName("「 Welcome");
-client.channels.find('id', ' 511254120485879848 ').setName("「 Welcome To");
-client.channels.find('id', ' 511254120485879848 ').setName("「 Welcome To D");
-client.channels.find('id', ' 511254120485879848 ').setName("「 Welcome To DJ");
-client.channels.find('id', ' 511254120485879848 ').setName("「 Welcome To DJ D");
-client.channels.find('id', ' 511254120485879848 ').setName("「 Welcome To DJ Da");
-client.channels.find('id', ' 511254120485879848 ').setName("「 Welcome To DJ DaM");
-client.channels.find('id', ' 511254120485879848 ').setName("「 Welcome To DJ DaM Se");
-client.channels.find('id', ' 511254120485879848 ').setName("「 Welcome To DJ DaM Serv ");
-client.channels.find('id', ' 511254120485879848 ').setName("「 Welcome To DJ DaM Server 」");
-  }, 3000);
+const Eris = require("eris");
+var x5bzteam = new Eris("process.env.BOT_TOKEN");
+x5bzteam.on("ready", () => {
+    console.log("Yo! Changer [ON]");
+    onstart();
 });
+x5bz = "507504573066379295";
+nick = "gamer kemar";
+function onstart(){
+    var v1 = nick.split('');
+    var counter;
+    var i=0;
+var x5bz2 = ' ';
+  var x5bz3 =   setInterval(function(){
+     
+     if (i == v1.length){
+     clearInterval(x5bz3);
+     onstart()
+      return;
+     }
+     var v2 = v1[i];
+    x5bz2 += v2;
+    x5bzteam.editNickname(x5bz, x5bz2);
+ 
+        i++;
+   
+     }, 1000);
+}
+x5bzteam.connect();
